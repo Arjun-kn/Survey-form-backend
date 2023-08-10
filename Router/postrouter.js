680b96b69c560fc4150dc01c9b49b1fae50ec931
Suresh Rathod <lingyanaikrathod7276@gmail.com>
2023-08-10 18:06:27 +0530

    Added code for delete and edit post

--- Router/postrouter.js
+++ Router/postrouter.js
@@ -50,4 +50,65 @@ postapp.post("/userpost", authenticateUser, (req, res) => {
   });
 });
 
+postapp.delete("/userpost/:postId", authenticateUser, (req, res) => {
+  jwt.verify(req.userId, process.env.Skey, (err, auth) => {
+    if (err) {
+      res.status(404).json({ message: "Authentication Failed" });
+    } else {
+      const postId = req.params.postId;
+
+         Postdata.findOneAndDelete({ _id: postId, user: auth._id })
+        .then((deletedRecord) => {
+          if (deletedRecord) {
+            res.status(200).json({ message: "Post deleted successfully" });
+          } else {
+            res.status(404).json({ message: "Post not found" });
+          }
+        })
+        .catch((err) => {
+          res.status(500).json({ message: "Failed to delete post" });
+        });
+    }
+  });
+});
+
+postapp.put("/userpost/:postId", authenticateUser, (req, res) => {
+  jwt.verify(req.userId, process.env.Skey, (err, auth) => {
+    if (err) {
+      res.status(404).json({ message: "Authentication Failed" });
+    } else {
+      const postId = req.params.postId;
+
+      Postdata.findOneAndUpdate(
+        { _id: postId, user: auth._id },
+        {
+          $set: {
+            Name: req.body.Name,
+            Description: req.body.Description,
+            Type: req.body.Type,
+            Start_Date: req.body.Start_Date,
+            End_Date: req.body.End_Date,
+            Action: req.body.Action,
+          },
+        },
+        { new: true }
+      )
+        .then((updatedRecord) => {
+          if (updatedRecord) {
+            res
+              .status(200)
+              .json({ message: "Post updated successfully", data: updatedRecord });
+          } else {
+            res.status(404).json({ message: "Post not found" });
+          }
+        })
+        .catch((err) => {
+          res.status(500).json({ message: "Failed to update post" });
+        });
+    }
+  });
+});
+
+
+
 module.exports = postapp;
