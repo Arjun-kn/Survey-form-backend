816a0af0d335abbf80f441aaa2d48de58aae18bf
arjun <arjunchauhan0009@gmail.com>
2023-08-10 15:06:05 +0530

    userpost schema and router crated

---
+++ Router/postrouter.js
@@ -0,0 +1,51 @@
+let express = require("express");
+let Postdata = require("../models/postmodel");
+require('dotenv').config()
+let authenticateUser = require("../middleware/middle");
+let jwt = require("jsonwebtoken");
+
+let postapp = express.Router();
+
+postapp.get("/userpost", authenticateUser, (req, res) => {
+  jwt.verify(userId, process.env.Skey, (err, auth) => {
+    if (err) {
+      res.status(404).json({ message: "Invalid token" });
+    } else {
+      Postdata.find({ user: auth.id })
+        .then((record) => {
+          res.status(200).json({ message: "Successfull", data: record });
+          console.log(record);
+        })
+        .catch((err) => {
+          res.status(500).json({ message: "Failed to fetch post" });
+        });
+    }
+  });
+});
+
+
+postapp.post("userpost",authenticateUser,(req,res)=>{
+    jwt.verify(userId,process.env.Skey,(err,auth)=>{
+        if(err){
+            res.status(404).json({message:"Authentication Failed"})
+        }else{
+          let newdata = new Postdata({
+            Name:req.body.Name,
+            Description:res.body.Description,
+            Type:res.body.Type,
+            Start_Date:req.body.Start_Date,
+            End_Date:req.body.End_Date,
+            Action:req.body.Action
+
+          })
+
+          newdata.save().then(record=>{
+            res.status(200).json({message:"Data post successfully", data:record})
+          }).catch(err=>{
+            res.status(404).json({message:"Failed to post data"})
+          })
+
+        }
+
+    })
+})
---
+++ middleware/middle.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization;
+// console.log(token)
+  if (token) {
+   
+    req.userId = token
+    next();
+  }else{
+    res.status(404).json({errdesc:"Authentication Failed"})
+
+  }
+
+  
+};
