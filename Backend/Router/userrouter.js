991e73249fe5154158aa996bce5280f830f59160
arjun <arjunchauhan0009@gmail.com>
2023-08-12 10:21:16 +0530

    status code added

--- Backend/Router/userrouter.js
+++ Backend/Router/userrouter.js
@@ -5,55 +5,52 @@ const jwt = require("jsonwebtoken");
 
 const userRouter = express.Router();
 const { register } = require("../models/usermodel");
+//......................................................
+
 userRouter.post("/register", async (req, res) => {
   const { name, email, phone, profession, password } = req.body;
-  if (!name || !email || !phone || !profession || !password) {
-    return res.send(`All fields are required`);
+
+  try {
+    const existingUser = await register.findOne({ email: email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = new register({
+      name,
+      email,
+      phone,
+      profession,
+      password: hashPassword,
+    });
+
+    await newUser.save();
+    return res.status(200).json({ message: "User registered successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
   }
-  register
-    .findOne({ email: email })
-    .then((user) => {
-      if (user) {
-        return res.send(`User already exists`);
-      } else {
-        bcrypt.hash(password, 10).then((hashPassword) => {
-          const user = new register({
-            name,
-            email,
-            phone,
-            profession,
-            password: hashPassword,
-          });
-          user
-            .save()
-            .then((newUser) => {
-              res.json({ message: "User registered successfully" });
-            })
-            .catch((err) => {
-              console.log(err.message);
-            });
-        });
-      }
-    })
-    .catch((err) => console.log(err.message));
 });
 
+
+
+//.................................................
 userRouter.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.json(`All fields are required`);
+    return res.status(400).json(`All fields are required`);
   }
   register
     .findOne({ email: email })
     .then((user) => {
       if (!user) {
-        return res.json(`User not fount`);
+        return res.status(404).json(`User not fount`);
       }
       bcrypt
         .compare(password, user.password)
         .then((isMatch) => {
           if (!isMatch) {
-            return res.json(`Invalid email/password`);
+            return res.status(404).json(`Invalid email/password`);
           }
           const token = jwt.sign(
             { id: user._id, email: user.email },
