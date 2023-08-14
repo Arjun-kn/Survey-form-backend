839f2e1af2ef9c8d10e70fdcfff46c10d6aacbd9
Kushal-Kake <kakekushalkumar369@gmail.com>
2023-08-14 15:03:38 +0530

    Added status codes in userrouter

--- Router/userrouter.js
+++ Router/userrouter.js
@@ -5,16 +5,17 @@ const jwt = require("jsonwebtoken");
 
 const userRouter = express.Router();
 const { register } = require("../models/usermodel");
+
 userRouter.post("/register", async (req, res) => {
   const { name, email, phone, profession, password } = req.body;
   if (!name || !email || !phone || !profession || !password) {
-    return res.send(`All fields are required`);
+    return res.status(400).send("All fields are required");
   }
   register
     .findOne({ email: email })
     .then((user) => {
       if (user) {
-        return res.send(`User already exists`);
+        return res.status(409).send("User already exists");
       } else {
         bcrypt.hash(password, 10).then((hashPassword) => {
           const user = new register({
@@ -27,42 +28,53 @@ userRouter.post("/register", async (req, res) => {
           user
             .save()
             .then((newUser) => {
-              res.json({ message: "User registered successfully" });
+              res.status(201).json({ message: "User registered successfully" });
             })
             .catch((err) => {
               console.log(err.message);
+              res.status(500).send("Internal Server Error");
             });
         });
       }
     })
-    .catch((err) => console.log(err.message));
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
 userRouter.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.json(`All fields are required`);
+    return res.status(400).json("All fields are required");
   }
   register
     .findOne({ email: email })
     .then((user) => {
       if (!user) {
-        return res.json(`User not fount`);
+        return res.status(404).json("User not found");
       }
       bcrypt
         .compare(password, user.password)
         .then((isMatch) => {
           if (!isMatch) {
-            return res.json(`Invalid email/password`);
+            return res.status(401).json("Invalid email/password");
           }
           const token = jwt.sign(
             { id: user._id, email: user.email },
             process.env.Skey
           );
-          res.json(token);
+          res.status(200).json(token);
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+        });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    });
 });
+
 module.exports = userRouter;
